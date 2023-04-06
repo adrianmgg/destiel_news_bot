2f7c75c1f6f5a4b7910e6b25e578b866f1c211ee
amgg <16143061+adrianmgg@users.noreply.github.com>
2023-04-06 12:22:07 -0700

    source list stuff, schema test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use reqwest::Url;
+use schemars::JsonSchema;
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize, JsonSchema)]
+enum NewsSource {
+    BBC { url: Url },
+    Reuters { url: Url },
+}
+
+#[derive(Debug, Deserialize, JsonSchema)]
+struct NewsSources {
+    sources: Vec<NewsSource>
+}
+
+
+
 fn main() {
     println!("Hello, world!");
+    let schema = schemars::schema_for!(NewsSources);
+    println!("{}", serde_json::to_string_pretty(&schema).unwrap());
 }
