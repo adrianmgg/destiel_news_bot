54ad83e98a4e273fbdcca643e8a4421d6cf2b904
amgg <16143061+adrianmgg@users.noreply.github.com>
2023-04-06 21:00:34 -0700

    stuff

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-
 use clap::Parser;
 use destielbot_rs::cli::Cli;
 use miette::{Context, IntoDiagnostic, Result};
@@ -10,20 +9,87 @@ use futures::StreamExt;
 use custom_debug::Debug;
 
 #[derive(Debug, Deserialize, JsonSchema)]
-enum NewsSource {
-    BBC {
-        #[debug(format = "{}")]
-        url: Url,
-    },
-    Reuters {
-        #[debug(format = "{}")]
-        url: Url,
-    },
+enum NewsSourceKind { BBC, Reuters }
+
+#[derive(Debug, Deserialize, JsonSchema)]
+struct NewsSource {
+    #[debug(format = "{}")]
+    url: Url,
+    kind: NewsSourceKind,
 }
 
 #[derive(Debug, Deserialize, JsonSchema)]
 struct NewsSources {
     sources: Vec<NewsSource>,
+}
+
+#[derive(Debug)]
+struct NewsStory {
+    id: String,
+    headline: String,
+    // TODO
+    // story_url: Url,
+}
+
+// from https://stackoverflow.com/a/69458453/8762161
+pub fn object_empty_as_none<'de, D, T>(deserializer: D) -> Result<Option<T>, D::Error>
+where
+    D: serde::Deserializer<'de>,
+    for<'a> T: Deserialize<'a>,
+{
+    #[derive(Deserialize, Debug)]
+    #[serde(deny_unknown_fields)]
+    struct Empty {}
+
+    #[derive(Deserialize, Debug)]
+    #[serde(untagged)]
+    enum Aux<T> {
+        T(T),
+        Empty(Empty),
+        Null,
+    }
+
+    match serde::Deserialize::deserialize(deserializer)? {
+        Aux::T(t) => Ok(Some(t)),
+        Aux::Empty(_) | Aux::Null => Ok(None),
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct BBCApiResponseAsset {
+    asset_id: String,
+    asset_uri: String,
+    headline: String,
+}
+
+#[derive(Debug, PartialEq, Eq, Deserialize)]
+struct BBCApiResponse {
+    #[serde(deserialize_with = "object_empty_as_none")]
+    asset: Option<BBCApiResponseAsset>,
+}
+
+#[cfg(test)]
+mod tests {
+    use serde_json::json;
+
+    use crate::{BBCApiResponse, BBCApiResponseAsset};
+
+    #[test]
+    fn decode_bbc_response_empty() {
+        assert_eq!(
+            Some(BBCApiResponse { asset: None }),
+            serde_json::from_value::<Option<BBCApiResponse>>(json!({"isError":false,"pollPeriod":30000,"asset":{}})).unwrap()
+        );
+    }
+
+    #[test]
+    fn decode_bbc_response_nonempty() {
+        assert_eq!(
+            Some(BBCApiResponse { asset: Some(BBCApiResponseAsset { asset_id: "1337".to_string(), asset_uri: "/news/uk-1337".to_string(), headline: "Hello World".to_string() }) }),
+            serde_json::from_value::<Option<BBCApiResponse>>(json!({"isError":false,"pollPeriod":30000,"asset":{"assetId":"1337","assetUri":"/news/uk-1337","headline":"Hello World"}})).unwrap()
+        );
+    }
 }
 
 #[tokio::main(flavor = "current_thread")]
@@ -39,6 +105,7 @@ async fn main() -> Result<()> {
             let mut schema_file = fs::OpenOptions::new()
                 .write(true)
                 .create(true)
+                .truncate(true)
                 .open(&schema_file_path)
                 .await
                 .into_diagnostic()
@@ -61,18 +128,23 @@ async fn main() -> Result<()> {
             let sources: NewsSources = serde_json::from_str(&sources_str)
                 .into_diagnostic()
                 .wrap_err("failed to parse sources list")?;
-            let mut stream = tokio_stream::iter(sources.sources)
-                .map(|source| {
-                    async move {
-                        tracing::info!("start {:?}", source);
-                        tokio::time::sleep(tokio::time::Duration::from_millis(2000)).await;
-                        tracing::info!("end   {:?}", source);
-                    }
-                })
-                .buffer_unordered(2);
-            while let Some(_) = stream.next().await {
-                // ...
-            }
+            // let client = reqwest::Client::new();
+            let client = reqwest::Client::builder().build().into_diagnostic()?;
+            let _stories: Vec<_> = tokio_stream::iter(
+                sources.sources.iter()
+                    .map(|source| {
+                        let req = client.get(source.url.clone()).send();
+                        async move {
+                            match req.await {
+                                Ok(resp) => {
+                                    tracing::info!("{:?}", resp.text().await.unwrap());
+                                    Some(NewsStory{ id: "TODO".to_string(), headline: "TODO".to_string() })
+                                },
+                                Err(_) => None,
+                            }
+                        }
+                    })
+            ).buffer_unordered(2).collect().await;
         }
     }
 
