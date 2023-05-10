8765d8b667d39d9624866f0bb033aab074a697ba
amgg <16143061+adrianmgg@users.noreply.github.com>
2023-05-10 13:07:32 -0700

    actually talk to the api

--- src/tumblr.rs
+++ src/tumblr.rs
@@ -64,8 +64,73 @@ pub async fn tumblr_auth_test(api_config: &TumblrApiConfig) -> Result<()> {
     Ok(())
 }
 
-pub async fn tumblr_api_test(api_config: &TumblrApiConfig) -> Result<()> {
-    let _client = setup_client(api_config)?;
+#[derive(Debug, Deserialize, Serialize)]
+pub struct ApiResponse<RT> {
+    pub meta: ApiResponseMeta,
+    pub response: RT,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct ApiResponseMeta {
+    /// "The 3-digit HTTP Status-Code (e.g., 200)"
+    pub status: i32,
+    /// "The HTTP Reason-Phrase (e.g., OK)"
+    pub msg: String,
+    /// unknown/unhandled fields
+    #[serde(flatten)]
+    pub other_fields: serde_json::Map<String, serde_json::Value>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct UserInfoResponse {
+    pub user: User,
+    /// unknown/unhandled fields
+    #[serde(flatten)]
+    pub other_fields: serde_json::Map<String, serde_json::Value>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct User {
+    /// "The number of blogs the user is following"
+    pub following: i64,
+    /// "The default posting format - html, markdown, or raw"
+    pub default_post_format: String, // TODO enum
+    /// "The user's tumblr short name"
+    pub name: String,
+    /// "The total count of the user's likes"
+    pub likes: i64,
+    /// "Each item is a blog the user has permissions to post to"
+    pub blogs: Vec<Blog>,
+    /// unknown/unhandled fields
+    #[serde(flatten)]
+    pub other_fields: serde_json::Map<String, serde_json::Value>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Blog {
+    /// "the short name of the blog"
+    pub name: String,
+    /// "the URL of the blog"
+    pub url: String,
+    /// "the title of the blog"
+    pub title: String,
+    /// "indicates if this is the user's primary blog"
+    pub primary: bool,
+    /// "total count of followers for this blog"
+    pub followers: i64,
+    /// "indicate if posts are tweeted auto, Y, N"
+    pub tweet: String, // TODO to bool
+    /// "indicates whether a blog is public or private"
+    #[serde(rename = "type")]
+    pub blog_type: String, // TODO enum
+    /// unknown/unhandled fields
+    #[serde(flatten)]
+    pub other_fields: serde_json::Map<String, serde_json::Value>,
+}
+
+
+pub async fn tumblr_api_test(_api_config: &TumblrApiConfig) -> Result<()> {
+    // let client = setup_client(api_config)?;
 
     let token_info_str = std::fs::read_to_string(".oauth2-token.json").into_diagnostic().wrap_err("failed to read saved token")?;
     let token_info: TokenInfo = serde_json::from_str(&token_info_str).into_diagnostic().wrap_err("failed to parse saved token")?;
@@ -73,6 +138,29 @@ pub async fn tumblr_api_test(api_config: &TumblrApiConfig) -> Result<()> {
     if token_info.is_expired()? {
         tracing::warn!("token appears to be expired!");
     }
+
+    let mut headers = reqwest::header::HeaderMap::new();
+    // headers.insert("Authorization", format!("Bearer {}", token_info.token_result));
+    // TODO - make sure it's actually a bearer one?
+    let mut auth_value = reqwest::header::HeaderValue::from_str(format!("Bearer {}", token_info.token_result.access_token().secret()).as_str())
+        .into_diagnostic()?;
+    auth_value.set_sensitive(true);
+    headers.insert(reqwest::header::AUTHORIZATION, auth_value);
+
+    let client = reqwest::Client::builder()
+        .default_headers(headers)
+        .build()
+        .into_diagnostic()?;
+
+    let user_info: ApiResponse<UserInfoResponse> = client.get("https://api.tumblr.com/v2/user/info")
+        .send()
+        .await
+        .into_diagnostic()?
+        .json()
+        .await
+        .into_diagnostic()?;
+
+    tracing::info!("{:#?}", user_info);
 
     Ok(())
 }
